e692fb2b95259a556a1c1938eb7421b0049875fc
lucascampos04 <camposdlucasoli@gmail.com>
2024-04-05 17:27:10 -0300

    Usando casting para converter double para int

---
+++ Main/src/Desafios/CastingDoubleParaInt/Main.java
@@ -0,0 +1,11 @@
+package Desafios.CastingDoubleParaInt;
+
+public class Main {
+    public static void main(String[] args) {
+        double peso = 80.5;
+        int pesoInteiro = (int) peso;
+
+        System.out.println("Seu peso em inteiro " + pesoInteiro);
+        System.out.println("Seu peso em double " + peso);
+    }
+}
