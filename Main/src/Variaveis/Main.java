ead41c877bc74ec5205285f3cc3e4215b06a4fa6
lucascampos04 <camposdlucasoli@gmail.com>
2024-04-05 16:17:31 -0300

    Variaveis

---
+++ Main/src/Variaveis/Main.java
@@ -0,0 +1,33 @@
+package Variaveis;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+         * Variaveis são tem que iniciar com a letra minuscula
+         * */
+
+        String nomeDoFilme = "Spiderman"; // String é um tipo de variável que aceita um texto
+        System.out.println("Filme: " + nomeDoFilme); // Imprime o valor da variável
+
+        int anoDeLancamento = 2010; // int é um tipo de variável que aceita apenas números inteiros
+        System.out.println("Ano de lancamento: " + anoDeLancamento); // Imprime o valor da variável
+
+        boolean isIncluidoNoPlano = true; // boolean é um tipo de variável que aceita apenas valores booleanos
+        System.out.println("Incluido no plano: " + isIncluidoNoPlano); // Imprime o valor da variável
+
+        float notaDoFilme = 8.5f; // float é um tipo de variável que aceita apenas números reais
+        System.out.println("(float) Nota do filme: " + notaDoFilme); // Imprime o valor da variável
+
+        double notaDoFilme2 = 9.5; // double é um tipo de variável que aceita apenas números reais
+        System.out.println("(double) Nota do filme: " + notaDoFilme2); // Imprime o valor da variável
+
+        /*
+        * int
+        * float
+        * double
+        * String
+        * boolean
+        * */
+    }
+}
