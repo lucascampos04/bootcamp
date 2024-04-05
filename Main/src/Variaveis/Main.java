3cdc4e197954a200c5409e6d4ebea74e9bfc9e59
lucascampos04 <camposdlucasoli@gmail.com>
2024-04-05 16:34:23 -0300

    Variaveis

--- Main/src/Variaveis/Main.java
+++ Main/src/Variaveis/Main.java
@@ -22,12 +22,29 @@ public class Main {
         double notaDoFilme2 = 9.5; // double é um tipo de variável que aceita apenas números reais
         System.out.println("(double) Nota do filme: " + notaDoFilme2); // Imprime o valor da variável
 
+        byte idade = 25; // byte é um tipo de variável que aceita apenas números inteiros entre -128 e 127
+        System.out.println("Idade: " + idade); // Imprime o valor da variável
+
+        char letra = 'a'; // char é um tipo de variável que aceita apenas um caractere
+        System.out.println("Letra: " + letra); // Imprime o valor da variável
+
+        short ano = 2022; // short é um tipo de variável que aceita apenas números inteiros entre -32768 e 32767
+        System.out.println("Ano: " + ano);
+
+        long populacao = 2000000000; // long é um tipo de variável que aceita apenas números inteiros
+        System.out.println("Populacao: " + populacao);
+
+
         /*
         * int
         * float
         * double
         * String
         * boolean
+        * byte
+        * char
+        * long
+        * short
         * */
     }
 }
