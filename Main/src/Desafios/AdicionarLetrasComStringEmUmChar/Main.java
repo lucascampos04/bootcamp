cb5b7eb5c382faf1e5eca8b8ac014e3a325b9b06
lucascampos04 <camposdlucasoli@gmail.com>
2024-04-05 17:26:18 -0300

    Concatenando char com string

---
+++ Main/src/Desafios/AdicionarLetrasComStringEmUmChar/Main.java
@@ -0,0 +1,10 @@
+package Desafios.AdicionarLetrasComStringEmUmChar;
+
+public class Main {
+    public static void main(String[] args) {
+        char o = 'O';
+        String nome = "Lucas";
+
+        System.out.println("Resultado = " + nome + " " + o + ".");
+    }
+}
