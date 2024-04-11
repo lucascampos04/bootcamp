3f91139b4261f912cb1a4ed1a5645a6a639b2933
lucascampos04 <camposdlucasoli@gmail.com>
2024-04-11 12:48:48 -0300

    Utilizando metodos

--- Main/src/Classe/Filme.java
+++ Main/src/Classe/Filme.java
@@ -3,7 +3,22 @@ package Classe;
 public class Filme {
     public String nome;
     public int anoLancamento;
-    public double avaliacao;
+    public double somaDasAvaliacoes;
     public boolean incluirNoPlano;
     public int totalAvaliacoes;
+
+
+    public void exibirInformacoes() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lancamento: " + anoLancamento);
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    public double mediaDasAvaliacoes(){
+        return somaDasAvaliacoes / totalAvaliacoes;
+    }
 }
--- Main/src/Main.java
+++ Main/src/Main.java
@@ -5,28 +5,33 @@ public class Main {
         int numeros = 0;
 
         // Tipo referencia
+        // instanciando o objeto
         Filme filme = new Filme(); // Cria um objeto da classe Filme
 
-        // instanciando o objeto
         filme.nome = "Vingadores";
         filme.anoLancamento = 2021;
-        filme.avaliacao = 9.5;
         filme.incluirNoPlano = true;
-        filme.totalAvaliacoes = 0;
 
 
         System.out.println("Nome do filme: " + filme.nome);
         System.out.println("Ano de lancamento: " + filme.anoLancamento);
-        System.out.println("Avaliação: " + filme.avaliacao);
-
-        while (numeros < 10) {
-            numeros++;
-        }
-        filme.totalAvaliacoes = numeros;
-
+        System.out.println("Avaliação: " + filme.somaDasAvaliacoes);
         System.out.println("Incluir no plano: " + filme.incluirNoPlano);
-        System.out.println("Total de avaliacoes: " + filme.totalAvaliacoes);
 
+        System.out.println("\n");
+        System.out.println("Ficha tecnica do filme: ");
+        usandoMetodo();
+    }
 
+    public static void usandoMetodo(){
+        Filme filme = new Filme();
+        filme.exibirInformacoes();
+        filme.avalia(10);
+        filme.avalia(7);
+        filme.avalia(4);
+        System.out.println("Avaliação: "+filme.somaDasAvaliacoes);
+        System.out.println("Total de avaliacoes: "+filme.totalAvaliacoes);
+        System.out.println("Media das avaliacoes: "+filme.mediaDasAvaliacoes());
     }
 }
+
