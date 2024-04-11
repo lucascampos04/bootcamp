9047849941d1552b37ccec9d1dde786afa4c89c9
lucascampos04 <camposdlucasoli@gmail.com>
2024-04-11 13:06:15 -0300

    Criar uma classe e exibir "Hello word" no terminal

---
+++ Main/src/Desafios/PessoaOlaMundo/Main.java
@@ -0,0 +1,8 @@
+package Desafios.PessoaOlaMundo;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        System.out.println(pessoa.olaMundo());
+    }
+}
---
+++ Main/src/Desafios/PessoaOlaMundo/Pessoa.java
@@ -0,0 +1,7 @@
+package Desafios.PessoaOlaMundo;
+
+public class Pessoa {
+    public String olaMundo() {
+        return "Ola Mundo!";
+    }
+}
